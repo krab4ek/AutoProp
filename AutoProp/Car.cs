ed0c42a893f1ccba95de314118cbab53ef9d28ef
Covali Andrei <krab4ek@gmail.com>
2022-07-01 21:37:14 +0300

    refresh

---
+++ AutoProp/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace AutoProp
+{
+    internal class Car
+    {
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Car name: {PetName}");
+            Console.WriteLine($"Speed: {Speed}");
+            Console.WriteLine($"Color: {Color}");
+
+        }
+
+
+    }
+}
--- AutoProp/Program.cs
+++ AutoProp/Program.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using AutoProp;
 
 Console.WriteLine("****** Fun with Automatic Properties ******\n");
- var c = new Car();
+Car? c = new();
 c.PetName = "Frank";
 c.Speed = 100;
 c.Color = "Orange";
@@ -11,18 +11,3 @@ c.DisplayStats();
 
 Console.ReadLine();
 
-class Car
-{
-    public string PetName { get; set; }
-    public int Speed { get; set; }
-    public string Color { get; set; }
-
-    public void DisplayStats()
-    {
-        Console.WriteLine($"Car name: {PetName}");
-        Console.WriteLine($"Speed: {Speed}");
-        Console.WriteLine($"Color: {Color}");
-
-    }
-
-}
