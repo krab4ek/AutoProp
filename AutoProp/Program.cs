0cb445d8c24ab6b9fa47aeeb214a7bf4ad180138
Covali Andrei <krab4ek@gmail.com>
2022-07-01 21:49:34 +0300

    final

--- AutoProp/Program.cs
+++ AutoProp/Program.cs
@@ -8,6 +8,9 @@ c.Color = "Orange";
 Console.WriteLine($"Your car is named {c.PetName}? That`s odd...");
 c.DisplayStats();
 
+Garage g = new Garage(c, 1);
+Console.WriteLine($"Numbers of Cars: {g.NumberOfCars}");
+Console.WriteLine(g.MyAuto.PetName);
 
 Console.ReadLine();
 
